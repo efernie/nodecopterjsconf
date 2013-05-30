b943a6594a99be3b5d6ae6c7ae904b0b6239d1f1
efernie <ericf2023@gmail.com>
2013-05-30 10:27:26 -0400

    set up sockets

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+    cons = require('consolidate'),
+    http = require('http'),
+    app = express(),
+    httpServer = http.Server(app),
+    io = require('socket.io').listen(httpServer);
+
+app.configure(function(){
+  app.set('view engine','html');
+  app.set('views', __dirname + '/views');
+
+  app.use(express.compress());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.favicon());
+
+  app.use(express.methodOverride());
+
+  app.use(express.static(__dirname + '/views'));
+  app.use(app.router);
+});
+
+httpServer.listen(3000, function () {
+  console.log( 'app listening on port 3000 for http - ENV:');
+});
+
+io.configure('development', function(){
+  io.set('log level', 1);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+
+// client.takeoff();
+
+// client
+//   .after(5000, function() {
+//     // this.clockwise(0.5);
+//   })
+//   .after(3000, function() {
+//     this.stop();
+//     this.land();
+//   });
